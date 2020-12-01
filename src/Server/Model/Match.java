96076f6b94fb9e861273643f48374cad9364323c
tconqueror_server <hoangnguyenduc.1101@gmail.com>
2020-12-01 16:30:31 +0000

    Release 1.0.5: Fix lỗi 0 điểm

--- src/Server/Model/Match.java
+++ src/Server/Model/Match.java
@@ -14,6 +14,9 @@ import java.io.Serializable;
 public class Match implements Serializable{
     private String player_1, player_2;
     private int score_1 = 0, score_2 = 0;
+    private boolean p1_updated = false;
+    private boolean p2_updated = false;
+    
 
     public Match(String player_1, String player_2) {
         this.player_1 = player_1;
@@ -39,17 +42,23 @@ public class Match implements Serializable{
         if (player.equals(player_1))
         {
             score_1 = p;
+            p1_updated = true;
             return true;
         }
         if (player.equals(player_2))
         {
             score_2 = p;
+            p2_updated = true;
             return true;
         }
         return false;
     }
     public String getWinner()
     {
+        if (!p1_updated)
+            return null;
+        if (!p2_updated)
+            return null;
         if (player_1.isEmpty() || player_2.isEmpty())
             return null;
         if (score_1 * score_2 == 0)
